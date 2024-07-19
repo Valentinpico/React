9ac14cb37943c124895ddb1d549bc8f7b2ac012e
valenitnpico <kwnigpico@gmail.com>
2024-07-19 01:05:33 -0500

    ASACXUTASDKLF

--- src/bd/roles.ts
+++ src/bd/roles.ts
@@ -15,84 +15,87 @@
   "cdo",
   "ciso", */
 //asistencia de personale por roles
-export const ROLES = [
-  {
-    name: "admin",
-    asist: 0,
-  },
+export const ROLES = {
+  date: "2021-09-01",
+  resgiter: [
+    {
+      name: "admin",
+      asist: 0,
+    },
 
-  {
-    name: "user",
-    asist: 0,
-  },
+    {
+      name: "user",
+      asist: 0,
+    },
 
-  {
-    name: "guest",
-    asist: 0,
-  },
+    {
+      name: "guest",
+      asist: 0,
+    },
 
-  {
-    name: "editor",
-    asist: 0,
-  },
+    {
+      name: "editor",
+      asist: 0,
+    },
 
-  {
-    name: "moderator",
-    asist: 0,
-  },
+    {
+      name: "moderator",
+      asist: 0,
+    },
 
-  {
-    name: "developer",
-    asist: 0,
-  },
+    {
+      name: "developer",
+      asist: 0,
+    },
 
-  {
-    name: "designer",
-    asist: 0,
-  },
+    {
+      name: "designer",
+      asist: 0,
+    },
 
-  {
-    name: "tester",
-    asist: 0,
-  },
+    {
+      name: "tester",
+      asist: 0,
+    },
 
-  {
-    name: "manager",
-    asist: 0,
-  },
+    {
+      name: "manager",
+      asist: 0,
+    },
 
-  {
-    name: "director",
-    asist: 0,
-  },
+    {
+      name: "director",
+      asist: 0,
+    },
 
-  {
-    name: "ceo",
-    asist: 0,
-  },
+    {
+      name: "ceo",
+      asist: 0,
+    },
 
-  {
-    name: "cto",
-    asist: 0,
-  },
+    {
+      name: "cto",
+      asist: 0,
+    },
 
-  {
-    name: "cfo",
-    asist: 0,
-  },
+    {
+      name: "cfo",
+      asist: 0,
+    },
 
-  {
-    name: "coo",
-    asist: 0,
-  },
+    {
+      name: "coo",
+      asist: 0,
+    },
 
-  {
-    name: "cdo",
-    asist: 0,
-  },
+    {
+      name: "cdo",
+      asist: 0,
+    },
 
-  {
-    name: "ciso",
-    asist: 0,
-  },
-];
+    {
+      name: "ciso",
+      asist: 0,
+    },
+  ],
+};
